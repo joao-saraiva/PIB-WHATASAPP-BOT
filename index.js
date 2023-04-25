165f9cb0c329515a851785eee49f1c646244ea9b
Joao Victor Saraiva <joao.saraiva@coreplan.com.br>
2023-04-25 13:54:58 -0300

    making the first api connection and using a post route

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const qrcode = require('qrcode-terminal');
 const myMock = require('./mock');
-
-import { defaultWrongText, helperText } from './mock.js'
+const axios = require("axios")
 
 const { Client, LocalAuth  } = require('whatsapp-web.js');
+
+const baseUrl = process.env.NODE_ENV == 'production' ? '' : 'localhost:3000/';
 
 const client = new Client({
   authStrategy: new LocalAuth()
@@ -19,11 +20,23 @@ client.on('ready', () => {
 
 client.on('message', message => {
   switch(message.body){
+    case '/inscrever':
+      const inscriptionData = { name: message.sender.name, number: message.from }
+
+      axios.post(`${baseUrl}/player_per_matches`, inscriptionData)
+        .then(() => {
+          message.reply(response.data);
+        })
+        .catch(() => {
+          message.reply(response.data);
+        });
+      break;
     case '/ajuda':
-      message.reply(helperText);
+      message.reply(myMock.helperText);
+      break;
     default:
       if(message.body.startsWith("/")){
-        message.reply(defaultWrongText);
+        message.reply(myMock.defaultWrongText);
       }
   }
 });
