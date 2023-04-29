649cd9dce27f590871bfef203324bc6038ddc97c
João Victor Saraiva <joaovsaraivasousa@gmail.com>
2023-04-29 17:12:32 -0300

    stabilisnh connect with the api via axios

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const qrcode = require('qrcode-terminal');
 const myMock = require('./mock');
-
-import { defaultWrongText, helperText } from './mock.js'
+const axios = require("axios");
 
 const { Client, LocalAuth  } = require('whatsapp-web.js');
+
+const url = process.env.MY_ENVIROMENT == "production" ? "teste" : "http://localhost:3000";
 
 const client = new Client({
   authStrategy: new LocalAuth()
@@ -19,11 +20,23 @@ client.on('ready', () => {
 
 client.on('message', message => {
   switch(message.body){
+    case '/inscrever':
+      message.getContact().then(contact => {
+        axios.post(`${url}/player_per_matches`, { player_name: contact.pushname, player_number: contact.number, status: "confirmed" } )
+          .then(response => {
+            message.reply(response.data);
+          })
+          .catch(error => {
+            message.reply("Não foi possivel fazer sua inscrição");
+          });
+      });
+      break;
     case '/ajuda':
-      message.reply(helperText);
+      message.reply(myMock.helperText);
+      break;
     default:
       if(message.body.startsWith("/")){
-        message.reply(defaultWrongText);
+        message.reply(myMock.defaultWrongText);
       }
   }
 });
